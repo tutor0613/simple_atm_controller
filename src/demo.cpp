48f7541f2650cc5a2e3803a0018e9f18e5380cca
Youngpyeong Kim <kyp970115@gmail.com>
2025-11-23 16:04:38 +0900

    apply: demo.cpp: Select menu structure

--- src/demo.cpp
+++ src/demo.cpp
@@ -1,14 +1,49 @@
 #include "atm_controller.hpp"
 
+using namespace std;
+
+enum Menu {BALANCE, DEPOSIT, WITHDRAW, NONE};
+
 int main() {
     try {
-        std::cout << "demo start" << std::endl;
-
         ATMController controller{};
-        std::cout << "controller connected : " << static_cast<int>(controller.getCardInfo("123","456")) << std::endl;
 
-    } catch (const std::exception& e) {
-        std::cout << "Except in main : " << e.what() << std::endl;
+        std::string input = "";
+        cout << "-------------------------------------------------------------" << endl;
+        cout << "  Welcome to Youngpyeong Kim's ATM Controller DEMO program.  " << endl << endl;
+        cout << "  Please select what you want :" << endl;
+        cout << "    - (1) : See Balance" << endl;
+        cout << "    - (2) : Deposit" << endl;
+        cout << "    - (3) : Withdraw" << endl;
+        cout << "    - (q) : quit" << endl << endl;
+        cout << "input ( ex: 1 ) : ";
+        cin >> input;
+        cout << endl;
+
+        Menu menu = NONE;
+        if (input == "1") menu = BALANCE;
+        else if (input == "2") menu = DEPOSIT;
+        else if (input == "3") menu = WITHDRAW;
+        else if (input == "q") cout << "Quit." << endl;
+        else cout << "Wrong input." << endl;
+
+        switch (menu) {
+            case BALANCE:
+                cout << "See Balance menu." << endl;
+                break;
+            case DEPOSIT:
+                cout << "Deposit menu." << endl;
+                break;
+            case WITHDRAW:
+                cout << "Withdraw menu." << endl;
+                break;
+            default:
+                cout << "Program will be terminated." << endl;
+                break;
+        }
+    }
+    catch (const exception& e) {
+        cout << "Except in main : " << e.what() << endl;
     }
 
     return 0;
