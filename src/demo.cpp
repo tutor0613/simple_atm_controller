e6675ce033fe6ae175fe712779d197810d6c9caa
tutor0613 <tutor0613@github.com>
2025-12-01 21:44:52 +0900

    refactor: Edit demo program to loop-executable structure

--- src/demo.cpp
+++ src/demo.cpp
@@ -12,39 +12,48 @@ void sleep(int ms) {  // [ms]
 }
 
 int main() {
-    try {
-        ATMController controller{};
+    ATMController controller{};
 
-        std::string input = "";
-        cout << "-------------------------------------------------------------" << endl;
-        cout << "  Welcome to Youngpyeong Kim's ATM Controller DEMO program.  " << endl << endl;
-        cout << "  Please select what you want :" << endl;
-        cout << "    - (1) : See Balance" << endl;
-        cout << "    - (2) : Deposit" << endl;
-        cout << "    - (3) : Withdraw" << endl;
-        cout << "    - (q) : quit" << endl << endl;
-        cout << "input ( ex: 1 ) : ";
-        cin >> input;
-        cout << endl;
+    while (true) {
+        try {
+            std::string input = "";
+            cout << "-------------------------------------------------------------" << endl;
+            cout << "  Welcome to Youngpyeong Kim's ATM Controller DEMO program.  " << endl << endl;
+            cout << "  Please select what you want :" << endl;
+            cout << "    - (1) : See Balance" << endl;
+            cout << "    - (2) : Deposit" << endl;
+            cout << "    - (3) : Withdraw" << endl;
+            cout << "    - (q) : quit" << endl << endl;
+            cout << "input ( ex: 1 ) : ";
+            cin >> input;
+            cout << endl;
 
-        Menu menu = NONE;
-        if (input == "1") {
-            menu = BALANCE;
-            cout << "See Balance menu." << endl;
-        }
-        else if (input == "2") {
-            menu = DEPOSIT;
-            cout << "Deposit menu." << endl;
-        }
-        else if (input == "3") {
-            menu = WITHDRAW;
-            cout << "Withdraw menu." << endl;
-        }
-        else if (input == "q") cout << "Quit." << endl;
-        else cout << "Wrong input." << endl;
+            Menu menu = NONE;
+            if (input == "1") {
+                menu = BALANCE;
+                cout << "See Balance menu." << endl;
+            }
+            else if (input == "2") {
+                menu = DEPOSIT;
+                cout << "Deposit menu." << endl;
+            }
+            else if (input == "3") {
+                menu = WITHDRAW;
+                cout << "Withdraw menu." << endl;
+            }
+            else if (input == "q") {
+                cout << "Quit." << endl;
+                break;
+            }
+            else {
+                cout << "Wrong input." << endl;
+            }
 
-        if (menu != NONE) {
-            string input;
+            if (menu == NONE) {
+                throw runtime_error("Invalid menu input.");
+            }
+
+            input = "";
             cout << "Insert your card. (Replace to get card number.)" << endl;
             cout << "Card number (ex: 12345678) : ";
             cin >> input;
@@ -112,9 +121,12 @@ int main() {
                     break;
             }
         }
-    }
-    catch (const exception& e) {
-        cout << e.what() << endl;
+        catch (const exception& e) {
+            cout << e.what() << endl;
+        }
+
+        cout << endl << "Return to the initial screen..." << endl;
+        sleep(1000);
     }
 
     cout << endl << "Program will be terminated." << endl;
